9a8a7a7fe9e3a864651367776b757a8b7262f20b
Achim Gsell <achim.gsell@psi.ch>
2007-11-22 10:39:18 +0000

    src/H5PartPrivate.h 	* declarations moved to corresponding files in libH5

--- src/H5PartPrivate.h
+++ src/H5PartPrivate.h
@@ -22,50 +22,41 @@ struct _iter_op_data {
 };
 
 h5part_int64_t
-_H5Part_set_step (
+H5_set_step (
 	H5PartFile *f,
 	const h5part_int64_t step
 	);
 
 h5part_int64_t
-_H5Part_get_num_particles (
+H5_get_num_particles (
 	H5PartFile *f
 	);
 
 herr_t
-_H5Part_iteration_operator (
+H5_iteration_operator (
 	hid_t group_id,
 	const char *member_name,
 	void *operator_data
 	);
 
 
-void
-_H5Part_set_funcname (
-	char * const fname
-	);
 
-const char *
-_H5Part_get_funcname (
-	void
-	);
-
-#define SET_FNAME( fname )	_H5Part_set_funcname( fname );
+#define SET_FNAME( fname )	H5_set_funcname( fname );
 
 hid_t
-_H5Part_normalize_h5_type (
+H5_normalize_h5_type (
 	hid_t type
 	);
 
 h5part_int64_t
-_H5Part_read_attrib (
+H5_read_attrib (
 	hid_t id,
 	const char *attrib_name,
 	void *attrib_value
 	);
 
 h5part_int64_t
-_H5Part_write_attrib (
+H5_write_attrib (
 	hid_t id,
 	const char *attrib_name,
 	const hid_t attrib_type,
@@ -74,7 +65,7 @@ _H5Part_write_attrib (
 	);
 
 h5part_int64_t
-_H5Part_get_attrib_info (
+H5_get_attrib_info (
 	hid_t id,
 	const h5part_int64_t attrib_idx,
 	char *attrib_name,
@@ -84,14 +75,14 @@ _H5Part_get_attrib_info (
 	);
 
 h5part_int64_t
-_H5Part_get_num_objects (
+H5_get_num_objects (
 	hid_t group_id,
 	const char *group_name,
 	const hid_t type
 	);
 
 h5part_int64_t
-_H5Part_get_num_objects_matching_pattern (
+H5_get_num_objects_matching_pattern (
 	hid_t group_id,
 	const char *group_name,
 	const hid_t type,
@@ -108,68 +99,6 @@ _H5Part_get_object_name (
 	const h5part_int64_t len_obj_name
 	);
 
-void
-_H5Part_vprint_error (
-	const char *fmt,
-	va_list ap
-	);
-
-void
-_H5Part_print_error (
-	const char *fmt,
-	... )
-#ifdef __GNUC__
-__attribute__ ((format (printf, 1, 2)))
-#endif
-;
-
-void
-_H5Part_vprint_warn (
-	const char *fmt,
-	va_list ap
-	);
-
-void
-_H5Part_print_warn (
-	const char *fmt,
-	...
-	)
-#ifdef __GNUC__
-__attribute__ ((format (printf, 1, 2)))
-#endif
-;
-
-void
-_H5Part_vprint_info (
-	const char *fmt,
-	va_list ap
-	);
-
-void
-_H5Part_print_info (
-	const char *fmt,
-	...
-	)
-#ifdef __GNUC__
-__attribute__ ((format (printf, 1, 2)))
-#endif
-;
-
-void
-_H5Part_vprint_debug (
-	const char *fmt,
-	va_list ap
-	);
-
-void
-_H5Part_print_debug (
-	const char *fmt,
-	...
-	)
-#ifdef __GNUC__
-__attribute__ ((format (printf, 1, 2)))
-#endif
-;
 
 char *
 _H5Part_strdupfor2c (
