bd3b9fa4f62fe054ef1f73057bd563948b72e72a
Achim Gsell <achim.gsell@psi.ch>
2008-02-14 15:38:56 +0000

    src/h5/t_readwrite.h - declaration of new functions added

--- src/h5/t_readwrite.h
+++ src/h5/t_readwrite.h
@@ -18,6 +18,39 @@ H5t_add_mesh (
 	);
 
 h5_id_t
+H5t_add_level (
+	h5_file * f
+	);
+
+h5_size_t
+H5t_add_num_vertices (
+	h5_file * f,
+	const h5_size_t num
+	);
+
+h5_id_t
+H5t_store_vertex (
+	h5_file * f,
+	const h5_id_t id,
+	const h5_float64_t P[3]
+	);
+
+h5_size_t
+H5t_add_num_tets (
+	h5_file * f,
+	const h5_size_t num
+	) ;
+
+h5_id_t
+H5t_store_tet (
+	h5_file * f,
+	const h5_id_t tet_id,		/*!< global tetrahedron id	*/
+	const h5_id_t parent_id,	/*!< global parent id
+					     if level \c >0 else \c -1	*/
+	const h5_id_t vertex_ids[4]	/*!< tuple with vertex id's	*/
+	);
+
+h5_id_t
 _h5t_open_mesh (
 	h5_file * f
 	);
