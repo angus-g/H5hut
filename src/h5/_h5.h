a972f67eba03531435618236121ec50668a13ace
Achim Gsell <achim.gsell@psi.ch>
2008-02-14 13:21:36 +0000

    src/h5/_h5.h - renamed, was "H5.h"

---
+++ src/h5/_h5.h
@@ -0,0 +1,14 @@
+#ifndef __H5_H
+#define __H5_H
+
+#include "h5_types.h"
+
+#include "attribs.h"
+#include "errorhandling.h"
+#include "openclose.h"
+#include "readwrite.h"
+#include "t_openclose.h"
+#include "t_readwrite.h"
+#include "u_readwrite.h"
+
+#endif
