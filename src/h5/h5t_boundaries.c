5b381c8defa1434d4fd0c78b5d033c345db3063b
Achim Gsell <achim.gsell@psi.ch>
2008-08-27 09:56:14 +0000

    added

---
+++ src/h5/h5t_boundaries.c
@@ -0,0 +1,12 @@
+#include <hdf5.h>
+
+#include "h5/h5_core.h"
+#include "h5/h5_core_private.h"
+
+h5_err_t
+h5t_add_boundary (
+	h5_file *f,
+	const char * const bname
+	) {
+	return -1;
+}
---
+++ src/h5/h5t_boundaries.h
@@ -0,0 +1,10 @@
+#ifndef __T_BOUNDARIES_H
+#define __T_BOUNDARIES_H
+
+h5_err_t
+h5t_add_boundary (
+	h5_file *f,
+	const char * const bname
+	);
+
+#endif
