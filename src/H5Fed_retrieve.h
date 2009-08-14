3eac0d6264ea28cc230fd3eeee7b7cbd71926cb3
Achim Gsell <achim.gsell@psi.ch>
2009-08-14 13:12:23 +0000

    see H5Fed_retrieve.c

--- src/H5Fed_retrieve.h
+++ src/H5Fed_retrieve.h
@@ -15,75 +15,19 @@
 #ifndef __H5FED_RETRIEVE_H
 #define __H5FED_RETRIEVE_H
 
-h5_err_t
-H5FedBeginTraverseVertices (
-	h5_file_t * const f
-	);
+h5_err_t H5FedBeginTraverseVertices ( h5_file_t * const f );
+h5_id_t H5FedTraverseVertices ( h5_file_t * const f, h5_float64_t P[3] );
+h5_err_t H5FedEndTraverseVertices ( h5_file_t * const f	);
 
-h5_id_t
-H5FedTraverseVertices (
-	h5_file_t * const f,
-	h5_id_t	* const	id,
-	h5_float64_t P[3]
-	);
+h5_err_t H5FedBeginTraverseEdges ( h5_file_t * const f );
+h5_id_t H5FedTraverseEdges ( h5_file_t * const f, h5_id_t * const vids );
+h5_err_t H5FedEndTraverseEdges ( h5_file_t * const f );
 
-h5_err_t
-H5FedEndTraverseVertices (
-	h5_file_t * const f
-	);
+h5_err_t H5FedBeginTraverseTriangles ( h5_file_t * const f );
+h5_id_t H5FedTraverseTriangles ( h5_file_t * const f, h5_id_t * const vids );
+h5_err_t H5FedEndTraverseTriangles ( h5_file_t * const f );
 
-h5_err_t
-H5FedBeginTraverseEdges (
-	h5_file_t * const f
-	);
-
-h5_id_t
-H5FedTraverseEdges (
-	h5_file_t * const f,
-	h5_id_t * const local_vids
-	);
-
-h5_err_t
-H5FedEndTraverseEdges (
-	h5_file_t * const f
-	);
-
-h5_err_t
-H5FedBeginTraverseTriangles (
-	h5_file_t * const f
-	);
-
-h5_id_t
-H5FedTraverseTriangles (
-	h5_file_t * const f,
-	h5_id_t * const local_vids
-	);
-
-h5_err_t
-H5FedEndTraverseTriangles (
-	h5_file_t * const f
-	);
-
-h5_err_t
-H5FedEndTraverseElements (
-	h5_file_t * const f
-	);
-
-h5_err_t
-H5FedBeginTraverseElements (
-	h5_file_t * const f
-	);
-
-h5_id_t
-H5FedTraverseElements (
-	h5_file_t * const f,
-	h5_id_t * const id,
-	h5_id_t * const parent_id,
-	h5_id_t * const local_vids
-	);
-
-h5_err_t
-H5FedEndTraverseElements (
-	h5_file_t * const f
-	);
+h5_err_t H5FedBeginTraverseElements ( h5_file_t * const f );
+h5_id_t H5FedTraverseElements ( h5_file_t * const f, h5_id_t * const vids );
+h5_err_t H5FedEndTraverseElements ( h5_file_t * const f );
 #endif
