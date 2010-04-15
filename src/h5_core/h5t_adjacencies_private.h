d2b696959c26ab4d3b580b445aebe12db01d0fd9
Achim Gsell <achim.gsell@psi.ch>
2010-04-15 16:05:16 +0000

    files removed

--- src/h5_core/h5t_adjacencies_private.h
+++
@@ -1,6 +0,0 @@
-#ifndef __H5T_ADJACENCIES_PRIVATE_H
-#define __H5T_ADJACENCIES_PRIVATE_H
-
-h5_err_t _h5t_rebuild_adj_data ( h5_file_t * const f );
-h5_err_t _h5t_release_adjacencies ( h5_file_t * const f );
-#endif
--- src/h5_core/h5t_boundaries_private.h
+++
@@ -1,6 +0,0 @@
-#ifndef __H5T_BOUNDARIES_PRIVATE_H
-#define __H5T_BOUNDARIES_PRIVATE_H
-
-#define H5T_BOUNDARYMESH_GRPNAME	"Boundaries"
-
-#endif
