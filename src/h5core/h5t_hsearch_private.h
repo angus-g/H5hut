8a3840baaee802ffec07fd10bb541ffee6141250
Achim Gsell <achim.gsell@psi.ch>
2010-05-02 20:44:17 +0000

    change return type of result to h5_id_list in h5t_find_xxx and h5t_search_xxx , use face_idx and elem_idx for face and element indices

--- src/h5core/h5t_hsearch_private.h
+++ src/h5core/h5t_hsearch_private.h
@@ -5,20 +5,32 @@ typedef struct h5t_te_entry_key {
 	h5_id_t vids[2];
 } h5t_te_entry_key_t;
 
+typedef struct h5t_td_entry_key {
+	h5_id_t vids[3];
+} h5t_td_entry_key_t;
+
+typedef struct h5t_idlisthash_key {
+	h5_id_t ids[1];
+} h5t_idlisthash_key_t;
+
+/*
+  List of all upward adjacent elements of same coarsness of a specific face.
+  The face is specified by its local vertex IDs.
+ */
 typedef struct h5_te_entry {
 	h5_idlist_t value;
 	h5t_te_entry_key_t key;
 } h5t_te_entry_t;
-
-typedef struct h5t_td_entry_key {
-	h5_id_t vids[3];
-} h5t_td_entry_key_t;
 
 typedef struct h5_td_entry {
 	h5_idlist_t value;
 	h5t_td_entry_key_t key;
 } h5t_td_entry_t;
 
+typedef struct h5t_idlisthash_entry {
+	h5_idlist_t value;
+	h5t_idlisthash_key_t key;
+} h5t_idlisthash_entry_t;
 
 h5_err_t
 h5tpriv_create_te_htab (
@@ -35,24 +47,24 @@ h5tpriv_resize_te_htab (
 h5_err_t
 h5tpriv_search_te2 (
 	h5_file_t * const f,
-	h5_id_t face_id,
-	h5_id_t local_eid,
-	h5t_te_entry_t **entry
+	h5_id_t face_idx,
+	h5_id_t elem_idx,
+	h5_idlist_t **entry
 	);
 
 h5_err_t
 h5tpriv_find_te (
 	h5_file_t * const f,
 	h5t_te_entry_t *item,
-	h5t_te_entry_t **retval
+	h5_idlist_t **retval
 	);
 
 h5_err_t
 h5tpriv_find_te2 (
 	h5_file_t * const f,
-	h5_id_t face_id,
-	h5_id_t local_eid,
-	h5t_te_entry_t **retval
+	h5_id_t face_idx,
+	h5_id_t elem_idx,
+	h5_idlist_t **retval
 	);
 
 
@@ -71,31 +83,31 @@ h5tpriv_resize_td_htab (
 h5_err_t
 h5tpriv_search_td2 (
 	h5_file_t * const f,
-	h5_id_t face_id,
-	h5_id_t local_eid,
-	h5t_td_entry_t **entry
+	h5_id_t face_idx,
+	h5_id_t elem_idx,
+	h5_idlist_t **entry
 	);
 
 h5_err_t
 h5tpriv_find_td (
 	h5_file_t * const f,
 	h5t_td_entry_t *item,
-	h5t_td_entry_t **retval
+	h5_idlist_t **retval
 	);
 
 h5_err_t
 h5tpriv_find_td2 (
 	h5_file_t * const f,
-	h5_id_t face_id,
-	h5_id_t local_eid,
-	h5t_td_entry_t **rentry
+	h5_id_t face_idx,
+	h5_id_t elem_idx,
+	h5_idlist_t **rentry
 	);
 
 h5_err_t
 h5tpriv_find_tv2 (
 	h5_file_t * const f,
-	h5_id_t cid,
-	h5_id_t el_idx,
+	h5_id_t face_idx,
+	h5_id_t elem_idx,
 	h5_idlist_t **retval
 	);
 #endif
