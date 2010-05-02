c72e04b302a2df6ac66535f62fb643f494e13257
Achim Gsell <achim.gsell@psi.ch>
2010-05-02 21:21:20 +0000

    added

---
+++ src/include/h5core/h5t_ref_elements.h
@@ -0,0 +1,19 @@
+#ifndef __H5T_REFERENCE_ELEMENTS_H
+#define __H5T_REFERENCE_ELEMENTS_H
+
+#define H5T_MAX_DIM 3
+#define H5T_MAX_FACES 6		// edges in tetrahedron
+#define H5T_MAX_VERTICES 4	// tetrahedron
+
+typedef struct {
+	int dim;
+	int num_faces[H5T_MAX_DIM+1];
+	int num_vertices_of_face[H5T_MAX_DIM+1][H5T_MAX_FACES];
+	int map[H5T_MAX_DIM+1][H5T_MAX_FACES][H5T_MAX_VERTICES];
+	h5_float64_t coords[H5T_MAX_VERTICES][H5T_MAX_DIM];
+} h5t_ref_element_t;
+
+extern const h5t_ref_element_t h5t_tet_ref_element;
+extern const h5t_ref_element_t h5t_tri_ref_element;
+
+#endif
