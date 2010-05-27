10f406da471fcb8e3148d4522729c1582d500637
Achim Gsell <achim.gsell@psi.ch>
2010-05-27 13:24:37 +0000

    adapted to coding style

--- src/h5core/h5t_errorhandling.c
+++ src/h5core/h5t_errorhandling.c
@@ -6,10 +6,10 @@
 #include "h5_core.h"
 #include "h5_core_private.h"
 
-
+const char* ERR_ELEM_NEXIST = "Element with local vertex IDs (%s) doesn't exist!";
 h5_err_t
 h5tpriv_error_local_elem_nexist (
-	h5_file_t * const f,
+	h5_file_t* const f,
 	h5_id_t local_vids[]
 	) {
 	h5t_fdata_t* t = f->t;
@@ -20,19 +20,14 @@ h5tpriv_error_local_elem_nexist (
 	int num_vertices = t->ref_element->num_faces[0];
 	for (i = 1; i < num_vertices; i++) {
 		num_chars_printed += snprintf (
-			s+num_chars_printed, sizeof(s)-num_chars_printed,
+			s + num_chars_printed, sizeof (s) - num_chars_printed,
 			"%lld,", local_vids[i]);
-		if ((sizeof(s) - num_chars_printed) < 32) {
+		if ((sizeof (s) - num_chars_printed) < 32) {
 			// buffer to small
 			return h5_error_internal (f, __FILE__, __func__, __LINE__);
 		}
 	}
 
-	return h5_error (
-		f,
-		H5_ERR_NOENTRY,
-		"Element with local vertex IDs "
-		"(%s) doesn't exist!",
-		s );
+	return h5_error (f, H5_ERR_NOENTRY, ERR_ELEM_NEXIST, s);
 }
 	
