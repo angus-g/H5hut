b56c4246c9f5072cc80a26531afaf1b9f06e159f
Achim Gsell <achim.gsell@psi.ch>
2010-07-22 10:16:22 +0000

    reference element type renamed

--- src/include/h5core/h5t_ref_elements.h
+++ src/include/h5core/h5t_ref_elements.h
@@ -11,9 +11,9 @@ typedef struct {
 	int num_vertices_of_face[H5T_MAX_DIM+1][H5T_MAX_FACES];
 	int map[H5T_MAX_DIM+1][H5T_MAX_FACES][H5T_MAX_VERTICES];
 	h5_float64_t coords[H5T_MAX_VERTICES][H5T_MAX_DIM];
-} h5t_ref_element_t;
+} h5t_ref_elem_t;
 
-extern const h5t_ref_element_t h5t_tet_ref_element;
-extern const h5t_ref_element_t h5t_tri_ref_element;
+extern const h5t_ref_elem_t h5t_tet_ref_elem;
+extern const h5t_ref_elem_t h5t_tri_ref_elem;
 
 #endif
