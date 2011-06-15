ade15f37caf76b8bb830fadab0eb7cc635060516
Achim Gsell <achim.gsell@psi.ch>
2011-06-15 13:30:11 +0000

    obsolete file 'h5t_errorhandling.c' removed

--- src/h5core/h5t_errorhandling.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>	/* va_arg - System dependent ?! */
-#include <string.h>
-#include <errno.h>
-#include "h5core/h5_core.h"
-#include "h5_core_private.h"
