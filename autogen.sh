339c86404c57915d98a6cb8fdd2405712be99ba3
Achim Gsell <achim.gsell@psi.ch>
2011-08-04 11:22:50 +0000

    fix problems with autogen.sh on ScientificLinux 5 and new autotools

--- autogen.sh
+++ autogen.sh
@@ -1,49 +1,48 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+ACLOCAL_FLAGS="-I m4 $ACLOCAL_FLAGS"
+LIBTOOLIZE_FLAGS="--force $LIBTOOLIZE_FLAGS"
+AUTOMAKE_FLAGS="--add-missing --copy --foreign $AUTOMAKE_FLAGS"
+
 LIBTOOLIZE=`which libtoolize`
 if [ "$LIBTOOLIZE" = "" ]; then
-	LIBTOOLIZE=`which glibtoolize`
+        LIBTOOLIZE=`which glibtoolize`
 fi
 if [ "$LIBTOOLIZE" = "" ]; then
-	echo "libtoolize not found" 1>&2
-	exit 1
+        echo "libtoolize not found" 1>&2
+        exit 1
 fi
 
 echo "+ making misc files ..."
 touch NEWS README AUTHORS ChangeLog
 echo
+libtoolize $LIBTOOLIZE_FLAGS || {
+	echo "libtoolize failed"
+	exit 1
+}
+echo
 echo "+ running aclocal ..."
 aclocal $ACLOCAL_FLAGS || {
-  echo
-  echo "aclocal failed - check that all needed development files are present on system"
-  exit 1
+	echo "aclocal failed - check that all needed development files are present on system"
+	exit 1
 }
 echo
 echo "+ running autoheader ... "
 autoheader || {
-  echo
-  echo "autoheader failed"
-  exit 1
-}
-echo
-echo "+ running libtoolize ... "
-$LIBTOOLIZE --force || {
-  echo
-  echo "libtoolize failed"
-  exit 1
+	echo "autoheader failed"
+	exit 1
 }
 echo
 echo "+ running autoconf ... "
 autoconf || {
-  echo
-  echo "autoconf failed"
-  exit 1
+	echo "autoconf failed"
+	exit 1
 }
 echo
 echo "+ running automake ... "
-automake -a -c --foreign || {
-  echo
-  echo "automake failed"
-  exit 1
+automake $AUTOMAKE_FLAGS || {
+	echo "automake failed"
+	exit 1
 }
+
